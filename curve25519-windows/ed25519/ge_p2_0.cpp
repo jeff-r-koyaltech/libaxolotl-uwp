b6564ffaa0101d31a800e82986adade4f04d2366
Jeff R <langboost@gmail.com>
2015-08-03 21:01:06 +0530

    Moving curve 25519 to github project langboost/curve25519-uwp.

--- curve25519-windows/ed25519/ge_p2_0.cpp
+++
@@ -1,9 +0,0 @@
-#include "pch.h"
-#include "ge.h"
-
-void ge_p2_0(ge_p2 *h)
-{
-  fe_0(h->X);
-  fe_1(h->Y);
-  fe_1(h->Z);
-}
--- curve25519-windows/ed25519/ge_precomp_0.cpp
+++
@@ -1,9 +0,0 @@
-#include "pch.h"
-#include "ge.h"
-
-void ge_precomp_0(ge_precomp *h)
-{
-  fe_1(h->yplusx);
-  fe_1(h->yminusx);
-  fe_0(h->xy2d);
-}
