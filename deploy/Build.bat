4b4e1a3ca7e636bda8095e61761b597884ed6aa4
Jeff R <langboost@gmail.com>
2015-10-27 14:55:02 +0530

    Switching libaxolotl nuget packaging back to AnyCPU and standardizing with how other PCL libs are deployed. fixes #11

--- deploy/Build.bat
+++ deploy/Build.bat
@@ -21,37 +21,17 @@ REM ======================= build =======================================
 
 REM Build out staging NuGet folder structure
 mkdir %StagingPath%
-mkdir %StagingPath%\build\uap\native
-REM mkdir %StagingPath%\lib\uap10.0
-mkdir %StagingPath%\runtimes\win10-arm\native
-mkdir %StagingPath%\runtimes\win10-x64\native
-mkdir %StagingPath%\runtimes\win10-x86\native
+mkdir %StagingPath%\lib\uap10.0
 
-REM Stage platform-independent files
-copy deploy\nuget\libaxolotl.targets %StagingPath%\build\uap\native
 
-REM build x86
-%MSBuildPath% libaxolotl\libaxolotl.csproj /property:Configuration=Release /property:Platform=x86
-REM stage x86
-copy libaxolotl\bin\x86\Release\libaxolotl.dll %StagingPath%\runtimes\win10-x86\native
-copy libaxolotl\bin\x86\Release\libaxolotl.pri %StagingPath%\runtimes\win10-x86\native
-
-REM build x64
-%MSBuildPath% libaxolotl\libaxolotl.csproj /property:Configuration=Release /property:Platform=x64
-REM stage x64
-copy libaxolotl\bin\x64\Release\libaxolotl.dll %StagingPath%\runtimes\win10-x64\native
-copy libaxolotl\bin\x64\Release\libaxolotl.pri %StagingPath%\runtimes\win10-x64\native
-
-REM build ARM
-%MSBuildPath% libaxolotl\libaxolotl.csproj /property:Configuration=Release /property:Platform=ARM
-REM stage ARM
-copy libaxolotl\bin\ARM\Release\libaxolotl.dll %StagingPath%\runtimes\win10-arm\native
-copy libaxolotl\bin\ARM\Release\libaxolotl.pri %StagingPath%\runtimes\win10-arm\native
+REM build Any CPU
+%MSBuildPath% libaxolotl\libaxolotl.csproj /property:Configuration=Release /property:Platform="AnyCPU"
+REM stage Any CPU (dll, pri, xml)
+copy libaxolotl\bin\Release\libaxolotl.* %StagingPath%\lib\uap10.0
 
 REM create NuGet package
-REM (Note: Expect 3 Assembly outside lib folder warnings, due to custom packaging technique)
 pushd deploy\staging
-%NuGetPath% pack ..\nuget\libaxolotl.nuspec -outputdirectory ..
+%NuGetPath% pack ..\nuget\libaxolotl-uwp.nuspec -outputdirectory ..
 popd
 
 
